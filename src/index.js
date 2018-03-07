60cb52421cd18d93ec346f21d1f701805c6cd281
instrumaniak <instrumaniak@gmail.com>
2018-03-07 14:16:59 +0600

    add node_sass for process scss files.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
 import {Provider, connect} from 'react-redux'
-import './index.css'
+import './index.scss'
 
 const initialState = {
   display:"0",
